d49cc3718bfe6393dda11c5377e20a1ca63f233d
D'angelo Nathan <nathan.dan@hotmail.fr>
2018-03-21 10:11:08 +0100

    connexion base de donnée

--- indexController.php
+++ indexController.php
@@ -5,42 +5,44 @@ use PHPMailer\PHPMailer\Exception ;
 use PHPMailer\PHPMailer\SMTP;
 include 'vendor/autoload.php';
 
-$receveur = $_POST["receveur"];
-
-
-$message = "";
-
 
 $mail = new PHPMailer(true); // instancie un new objet phpmailer
 
-try {
-    
+    $body  =  file_get_contents ( ' contents.html ' );
+
     //$mail->SMTPDebug = 2 ;
     $mail->isSMTP();
     
     $mail->Host = 'smtp.gmail.com';
     $mail->SMTPAuth  =  true ;
+    $mail->SMTPKeepAlive  =  true ;
     $mail->Username = 'zegick.laforme@gmail.com';
     $mail->Password = 'pingoleon42400';
     $mail->SMTPSecure = 'tls';
     $mail->Port = 587;
     $mail->setFrom("colibri@gmail.com");
-    $mail->addAddress($receveur);     // Add a recipient
-    
-      // Add attachments
-    
-    
-    $mail->isHTML(true);                                  // Set email format to HTML
     $mail->Subject = 'Planning de la Semaine';
-    $mail->msgHTML (file_get_contents('contents.html'), __DIR__);
+    $mail->msgHTML($body);
     $mail->AltBody = 'Bienvenue a tous';
-    $mail->send();
-    echo 'Message Envoyé';
     
-}
-catch (Exception $e) {
-    echo 'Message non envoyé ' , $mail->ErrorInfo;
-}
+    
+    
+    $mysql = mysqli_connect('localhost', 'Borne', 'il7o70jq6KQRFYpe');
+    mysqli_select_db($mysql, 'mydb');
+    $result = mysqli_query($mysql, 'SELECT mail FROM subscribers WHERE sent = FALSE');
+    foreach ($result as $row) {
+        $mail->addAddress($row['mail']);    
+        }
+        if (!$mail->send()) {
+            echo "Mail Error (" . str_replace("@", "&#64;", $row["mail"]) . ') ' . $mail->ErrorInfo . '<br />';
+            break; //Abandon sending
+        } else {
+            echo "Message sent to :" . $row['mail'] . ')<br />';
+            
+            
+        }   
+
+
 require 'indexView.php';
 
 ?>
